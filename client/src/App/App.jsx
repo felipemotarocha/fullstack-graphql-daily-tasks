cd9e899f575c7de1c6b3a88e122442f16c455e08
Felipe Rocha <felipemota.rocha@gmail.com>
2020-08-03 12:56:55 -0300

    Client: Initial commit

---
+++ client/src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./App.styles";
+
+const App = () => {
+	return (
+		<Container>
+			<h1>hello world!</h1>
+		</Container>
+	);
+};
+
+export default App;
---
+++ client/src/App/App.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	box-sizing: border-box;
+	height: 100vh;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+		Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
